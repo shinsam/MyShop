4eb50a415d695f829a24fbba9e9f0e3739ae7abd
shinsam <shin321gg@gmail.com>
2022-05-27 12:18:50 +0900

    이미지 추가

--- app/src/main/java/com/example/myshop/GuideActivity.java
+++ app/src/main/java/com/example/myshop/GuideActivity.java
@@ -2,7 +2,10 @@ package com.example.myshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class GuideActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class GuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+
+        findViewById(R.id.img_map).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //묵시적 인텐트
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("geo:37.4807426,127.084375?z=16"));
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/myshop/InfoActivity.java
+++ app/src/main/java/com/example/myshop/InfoActivity.java
@@ -3,14 +3,52 @@ package com.example.myshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class InfoActivity extends AppCompatActivity {
+
+    int no = 0 ; //그림 번호 : 0 1 2 3 4 0 1 2 3 4
+    int imgs[] = {R.drawable.info1, R.drawable.info2, R.drawable.info3,
+                    R.drawable.info4, R.drawable.info5} ;
+    String txts[] = {"1번 그림 설명", "2번 그림 설명","3번 그림 설명",
+            "4번 그림 설명","5번 그림 설명"};
+    ImageView img_info;
+    TextView  txt_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        //test
+        findViewById(R.id.btn_info_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        //이미지 처리 - 초기화
+        img_info = findViewById(R.id.img_info);
+        txt_info = findViewById(R.id.txt_info);
+
+        img_info.setImageResource(imgs[0]);
+        txt_info.setText(txts[0]);
+
+        img_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(InfoActivity.this, "이미지 클릭", Toast.LENGTH_SHORT).show();
+
+                no = ++no % 5 ;
+                img_info.setImageResource(imgs[no]);
+                txt_info.setText(txts[no]);
+            }
+        });
+
+
+
     }
 }
